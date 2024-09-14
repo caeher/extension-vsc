05a8a999e467fc06ccbee34a6bf8656c8526dd18
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2024-09-14 16:04:55 -0600

    feat: agregar N lineas en blanco a texto seleccionado.

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand(
+        "extension.gapline",
+        () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) return;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gapline" is now active!');
+            const selection = editor.selection;
+            let text = editor.document.getText(selection);
+            vscode.window.showInputBox({ prompt: "Lineas?" }).then((value) => {
+                let numberOfLines = +value;
+                var textInChunks: Array<string> = [];
+                text.split("\n").forEach((currentLine: string, lineIndex) => {
+                    textInChunks.push(currentLine);
+                    if ((lineIndex + 1) % numberOfLines === 0)
+                        textInChunks.push("");
+                });
+                text = textInChunks.join("\n");
+                editor.edit((editBuilder) => {
+                    var range = new vscode.Range(
+                        selection.start.line,
+                        0,
+                        selection.end.line,
+                        editor.document.lineAt(selection.end.line).text.length
+                    );
+                    editBuilder.replace(range, text);
+                });
+            });
+        }
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gapline.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Line Gapper!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
